e1962fc80446d5ba9d9789363b177eb2fe890101
Daniel Silvestre <136662533+0byrdj1n@users.noreply.github.com>
2023-07-27 23:25:04 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+let employee = {
+  name: "John Doe",
+  streetAddress: "123 Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let result = { ...employee };
+  delete result[key];
+  return result;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
